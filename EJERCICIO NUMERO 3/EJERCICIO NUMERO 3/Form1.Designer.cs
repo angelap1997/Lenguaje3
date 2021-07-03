2c9d8d82885b0baf982136274f043939f6f9b20d
angelap1997 <86858899+angelap1997@users.noreply.github.com>
2021-07-02 19:16:23 -0600

    Primer comit
    Ejercicio 5 Matrices utilizando la clase Ramdon

---
+++ EJERCICIO NUMERO 3/EJERCICIO NUMERO 3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace EJERCICIO_NUMERO_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.FilastextBox = new System.Windows.Forms.TextBox();
+            this.ColumtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FILAS";
+            // 
+            // FilastextBox
+            // 
+            this.FilastextBox.Location = new System.Drawing.Point(34, 91);
+            this.FilastextBox.Name = "FilastextBox";
+            this.FilastextBox.Size = new System.Drawing.Size(100, 20);
+            this.FilastextBox.TabIndex = 1;
+            // 
+            // ColumtextBox
+            // 
+            this.ColumtextBox.Location = new System.Drawing.Point(206, 91);
+            this.ColumtextBox.Name = "ColumtextBox";
+            this.ColumtextBox.Size = new System.Drawing.Size(100, 20);
+            this.ColumtextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "COLUMNAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "X";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "EJECUTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 226);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ColumtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FilastextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FilastextBox;
+        private System.Windows.Forms.TextBox ColumtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EJERCICIO NUMERO 3/EJERCICIO NUMERO 3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO_NUMERO_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //LLENAR
+            int FILA = Convert.ToInt32(FilastextBox.Text);
+            int COLUMNA = Convert.ToInt32(ColumtextBox.Text);
+
+            int[,] MatrizAle = new int[FILA, COLUMNA];
+            Random aleotorio = new Random();
+
+            for (int fila = 0; fila < MatrizAle.GetLength(0); fila++)
+            {
+                for (int colum = 0; colum < MatrizAle.GetLength(0); colum++)
+                {
+                    MatrizAle[fila, colum] = aleotorio.Next(100);
+                }
+            }
+            ////
+            for (int fila = 0; fila < MatrizAle.GetLength(0); fila++)
+            {
+                for (int colum = 0; colum < MatrizAle.GetLength(0); colum++)
+                {
+                    MessageBox.Show("La pocicion es:" + fila + "," + colum + "=" + MatrizAle[fila, colum].ToString());
+                }
+            }
+        }
+    }
+}
